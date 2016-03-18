366b486f586fd2e3d77aff5f236782d181326fe1
Aldo Castro <aldo.castro21@gmail.com>
2016-03-18 23:35:29 +0100

    implementing BookModel structure and its Factory class

--- BookFinder/BookFinder/BookModel.swift
+++ BookFinder/BookFinder/BookModel.swift
@@ -7,3 +7,67 @@
 //
 
 import Foundation
+
+struct BookModel {
+    var title: String
+    var authors: [[String: String]]
+    var cover: String
+    
+    func authorsDescription() -> String {
+        var description = ""
+        
+        if authors.count > 1 {
+            for author in authors {
+                description += "\(author["name"]), "
+            }
+        } else {
+            if let firstAuthor = authors.first as [String : String]!, author = firstAuthor["name"] as String! {
+                description = author
+            }
+        }
+        
+        return description
+    }
+    
+    func coverUrl() -> NSURL? {
+        guard cover != "" else { return nil }
+        
+        if let scapedStr = cover.stringByAddingPercentEncodingWithAllowedCharacters(.URLQueryAllowedCharacterSet()),
+            url = NSURL(string: scapedStr) as NSURL! {
+            return url
+        }
+        
+        return nil
+    }
+}
+
+class BookModelFactory {
+    
+    static func createBookModelFromDictionary(jsonDictionary: [String : AnyObject]) -> BookModel {
+        var title = ""
+        var authors = [[String:String]]()
+        var cover = ""
+        
+        if let key = jsonDictionary.keys.first, bookDict = jsonDictionary[key] {
+            
+            if let _title = bookDict["title"] as? String {
+                title = _title
+                print("title: \(title)")
+            }
+            
+            if let _authors = bookDict["authors"] as? [[String : String]] {
+                authors = _authors
+                print("authors: \(authors)")
+            }
+            
+            if let coverDict = bookDict["cover"] as? [String:String],
+                largeCoverStr = coverDict["medium"] as String! {
+                cover = largeCoverStr
+                print("url: \(cover)")
+            }
+        }
+        
+        return BookModel(title: title, authors: authors, cover: cover)
+    }
+    
+}
