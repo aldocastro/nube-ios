16714264da59ddc0cf8f32e5f1fda1910aea0e33
Aldo Castro <aldo.castro21@gmail.com>
2016-03-24 02:33:28 +0100

    adding isbn property to model objects

--- BookFinder/BookFinder/Book+CoreDataProperties.swift
+++ BookFinder/BookFinder/Book+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Book+CoreDataProperties.swift
 //  BookFinder
 //
-//  Created by Aldo Castro on 23/03/16.
+//  Created by Aldo Castro on 24/03/16.
 //  Copyright © 2016 Aldo Castro. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -14,8 +14,9 @@ import CoreData
 
 extension Book {
 
-    @NSManaged var title: String?
     @NSManaged var cover: String?
+    @NSManaged var title: String?
+    @NSManaged var isbn: String?
     @NSManaged var authors: NSSet?
 
 }
--- BookFinder/BookFinder/BookModel.swift
+++ BookFinder/BookFinder/BookModel.swift
@@ -9,12 +9,14 @@
 import Foundation
 
 struct BookModel {
+    var isbn: String
     var title: String
     var authors: [[String: String]]
     var cover: String
     
     func authorsDescription() -> String {
         var description = ""
+        
         
         if authors.count > 1 {
             for author in authors {
@@ -44,11 +46,16 @@ struct BookModel {
 class BookModelFactory {
     
     static func createBookModelFromDictionary(jsonDictionary: [String : AnyObject]) -> BookModel {
+        var isbn = ""
         var title = ""
         var authors = [[String:String]]()
         var cover = ""
         
         if let key = jsonDictionary.keys.first, bookDict = jsonDictionary[key] {
+            
+            if let _isbn = bookDict["isbn_13"] as? [String], firstItem = _isbn.first {
+                isbn = firstItem
+            }
             
             if let _title = bookDict["title"] as? String {
                 title = _title
@@ -64,7 +71,7 @@ class BookModelFactory {
             }
         }
         
-        return BookModel(title: title, authors: authors, cover: cover)
+        return BookModel(isbn: isbn, title: title, authors: authors, cover: cover)
     }
     
 }
